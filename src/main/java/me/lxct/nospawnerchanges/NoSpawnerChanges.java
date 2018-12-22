481ab6dca9cc960249ad4e18ab91fa8d66db22a2
Lxct <sofiane.djerbi@yahoo.com>
2018-12-22 14:46:04 +0100

    Initial Commit

--- src/main/java/me/lxct/nospawnerchanges/NoSpawnerChanges.java
+++ src/main/java/me/lxct/nospawnerchanges/NoSpawnerChanges.java
@@ -3,10 +3,13 @@ package me.lxct.nospawnerchanges;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import static me.lxct.nospawnerchanges.Variables.loadVariables;
+
 public class NoSpawnerChanges extends JavaPlugin implements Listener {
     static NoSpawnerChanges plugin;
     public void onEnable() {
         plugin = this;
+        loadVariables();
         getServer().getPluginManager().registerEvents(new OnSpawnerChange(), this);
     }
 }
